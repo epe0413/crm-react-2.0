0f652fee001d5850ff05037358f8939c12fab5c9
Everth Paredes <epe0413@gmail.com>
2023-06-22 17:25:40 -0600

    Mostrando el Resto de la Información

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -3,9 +3,24 @@ const Cliente = ({cliente}) => {
 
     const { nombre, empresa, email, telefono, id } = cliente
     return (
-        <tr className="hover:bg-cyan-50">
+        <tr className="hover:bg-gray-100 border-b">
+            <td className='p-2 space-y-1'>
+                <p className="text-lg text-gray-800">{nombre}</p>
+                <p>{empresa}</p>
+            </td>
             <td className='p-2'>
-                {nombre}
+                <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">EMAIL: </span>{email}</p>
+                <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
+            </td>
+            <td className='p-2 flex gap-2 justify-center'>
+                <button
+                    type="button"
+                    className="text-cyan-600 hover:text-cyan-700 uppercase font-bold text-xs"
+                >Editar</button>
+                <button
+                    type="button"
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+                >Eliminar</button>
             </td>
         </tr>
     )
