27df703cb711dc46e8065e3ff28838cf5d265b44
Everth Paredes <epe0413@gmail.com>
2023-06-22 12:11:12 -0600

    Navegar entre Componentes con Link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 const Layout = () => {
     return (
         <div className='md:flex md:min-h-screen'>
             <aside className='md:w-1/5 bg-cyan-800 px-5 py-5'>
                 <h2 className='text-2xl font-black text-center text-white'>CRM - Cliente</h2>
+
+                <nav className='mt-10'>
+                    {/* <a href="/">Clientes</a>
+                    <a href="/clients/new">Nuevo Clientes</a> */}
+                    <Link className='text-xl block mt-2 hover:text-blue-300 text-white' to="/">Clientes</Link>
+                    <Link className='text-xl block mt-2 hover:text-blue-300 text-white' to="/clients/new">Nuevo Clientes</Link>
+
+                </nav>
             </aside>
             <div className="md:w-4/5 p-5 md:h-screen overflow-scroll">
                 <Outlet/>
